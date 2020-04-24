9150a20d5c4d459e4d3f0a9a2fae1000bd5844f2
Maxime PIOC <maxime.pioc@havasdigitalfactory.com>
2020-04-24 16:20:46 +0200

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t maxpicky/multi-client-k8s:latest -t maxpicky/multi-client-k8s:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t maxpicky/multi-server-k8s:latest -t maxpicky/multi-server-k8s:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t maxpicky/multi-worker-k8s:latest -t maxpicky/multi-worker-k8s:$GIT_SHA -f ./worker/Dockerfile ./worker
+
+docker push maxpicky/multi-client-k8s:latest
+docker push maxpicky/multi-server-k8s:latest
+docker push maxpicky/multi-worker-k8s:latest
+
+docker push maxpicky/multi-client-k8s:$GIT_SHA
+docker push maxpicky/multi-server-k8s:$GIT_SHA
+docker push maxpicky/multi-worker-k8s:$GIT_SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=stephengrider/multi-server:$GIT_SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$GIT_SHA
